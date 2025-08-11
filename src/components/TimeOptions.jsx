a02c56f83ce295d3d1a8d89c4957180dfdb6dbf7
abdfthsaid <abdifatahsaedmxm@gmail.com>
2025-08-11 17:19:37 +0300

    Update TimeOptions.jsx

--- src/components/TimeOptions.jsx
+++ src/components/TimeOptions.jsx
@@ -51,7 +51,7 @@ const TimeOptions = ({ selectedAmount, selectTime }) => {
           <br />
           Taleex
         </h1>
-        <p className="text-sm font-light">Dooro muddoada kugu habboon</p>
+        <p className="text-sm font-light">Dooro Muddada kugu habboon</p>
       </div>
 
       <div className="flex justify-between gap-3 mt-6 ml-3 mr-3">
